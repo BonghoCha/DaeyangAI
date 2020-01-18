0976df40f473d1b3c59278426450e4d3465c7daf
BonghoCha <ho9354@naver.com>
2020-01-18 17:26:22 +0900

    ADD add crawling
    - Crawling.cs = Crawling & Parsing

---
+++ Assets/Crawling.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Crawling : MonoBehaviour
+{
+    void Start()
+    {
+        // A correct website page.
+        StartCoroutine(GetRequest("http://board.sejong.ac.kr/boardlist.do?bbsConfigFK=333"));
+
+        // A non-existing page.
+        StartCoroutine(GetRequest("https://error.html"));
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                GameObject.Find("Debug").GetComponent<Text>().text = webRequest.downloadHandler.text;
+                Debug.Log(webRequest.downloadHandler.text.IndexOf("date"));
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
