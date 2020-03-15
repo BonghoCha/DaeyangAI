b019070eeb10a20e6f88c23a9c1a3aa435909da9
hon99oo <cocoti5797@gmail.com>
2020-03-15 16:22:40 +0900

    ADD change page

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	public GameObject[] m_page_icons;
+
+	public void ChangePage(int current)
+	{
+		for (var i = 0; i < m_page_icons.Length; i++)
+		{
+			m_page_icons[i].SetVisible(false);
+		}
+		m_page_icons[current].SetVisible(true);
+	}
+
+	private void Start()
+	{
+
+	}
+
+    private void Update()
+	{
+
+	}
+}
+
