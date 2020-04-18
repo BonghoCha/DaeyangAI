14f506bea8f162dac53f13c74367a1a5b306553d
hon99oo <kanghonggoo@naver.com>
2020-04-18 15:35:55 +0900

    ADD google map

---
+++ Assets/02.Scripts/Google Maps/GoogleStaticMap.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoogleStaticMap : MonoBehaviour
+{
+	public RawImage rawImage;
+	[Range(0f, 1f)]
+	public float transparency = 1f;
+	public float mapCenterLatitude = 37.5665350f;
+	public float mapCenterLongtitude = 126.9779690f;
+	[Range(1, 20)]
+	public int mapZoom = 14;
+	public int mapWidth = 640;
+	public int mapHeight = 640;
+	public enum MapType
+	{
+		roadmap, satellite, terrain, hybrid,
+	}
+	public MapType mapType = MapType.roadmap;
+	[Range(1, 4)]
+	public int scale = 1;
+	public float markerLatitude = 37.5665350f;
+	public float markerLongtitude = 126.9779690f;
+	public enum MarkerSize
+	{
+		tiny, mid, small,
+	}
+	public MarkerSize markerSize = MarkerSize.mid;
+	public enum MarkerColor
+	{
+		black, brown, green, purple, yellow, blue, gray, orange, red, white,
+	}
+	public MarkerColor markerColor = MarkerColor.blue;
+	public char label = 'C';
+	public string apiKey;
+
+	private string url;
+	private Color rawImageColor = Color.white;
+
+	IEnumerator Map()
+	{
+		rawImageColor.a = transparency;
+		rawImage.color = rawImageColor;
+
+		label = Char.ToUpper(label);
+
+		url = "https://maps.googleapis.com/maps/api/staticmap"
+			+ "?center=" + mapCenterLatitude + "," + mapCenterLongtitude
+			+ "&zoom=" + mapZoom
+			+ "&size=" + mapWidth + "x" + mapHeight
+			+ "&scale=" + scale
+			+ "&maptype=" + mapType
+			+ "&markers=size:" + markerSize + "%7Ccolor:" + markerColor + "%7Clabel:" + label + "%7C" + markerLatitude + "," + markerLongtitude
+			+ "&key=" + apiKey;
+		WWW www = new WWW(url);
+		yield return www;
+		rawImage.texture = www.texture;
+		rawImage.SetNativeSize();
+	}
+
+	public void RefreshMap()
+	{
+		if (gameObject.activeInHierarchy)
+		{
+			StartCoroutine(Map());
+		}
+	}
+
+	private void Reset()
+	{
+		rawImage = gameObject.GetComponentInChildren<RawImage>();
+		RefreshMap();
+	}
+
+	private void Start()
+	{
+		Reset();
+	}
+
+#if UNITY_EDITOR
+	private void OnValidate()
+	{
+		RefreshMap();
+	}
+#endif
+}
---
+++ Assets/GoogleApi.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoogleApi : MonoBehaviour
+{
+    public RawImage img;
+
+    string url;
+
+    public float lat;
+    public float lon;
+
+    LocationInfo li;
+
+    public int zoom = 20;
+    public int mapWidth = 640;
+    public int mapHeight = 640;
+
+    public enum mapType { roadmap, satellite, hybrid, terrain }
+    public mapType mapSelected;
+    public int scale;
+
+    public void SetLocation(int number)
+    {
+        if(number == 1)
+        {
+            lat = 37.548824f; 
+            lon = 127.074574f;
+        }
+        else if (number == 2)
+        {
+            lat = 37.549108f;
+            lon = 127.073766f;
+        }
+        else if (number == 3)
+        {
+            lat = 37.549449f;
+            lon = 127.075064f;
+        }
+        else if (number == 4)
+        {
+            lat = 37.549555f;
+            lon = 127.073678f;
+        }
+        else if (number == 5)
+        {
+            lat = 37.549938f;
+            lon = 127.074579f;
+        }
+        else if (number == 6)
+        {
+            lat = 37.550019f;
+            lon = 127.073283f;
+        }
+        else if (number == 7)
+        {
+            lat = 37.550507f;
+            lon = 127.072948f;
+        }
+        else if (number == 8)
+        {
+            lat = 37.550806f;
+            lon = 127.073910f;
+        }
+        else if (number == 9)
+        {
+            lat = 37.550890f;
+            lon = 127.073236f;
+
+        }
+        else if (number == 10)
+        {
+            lat = 37.551299f;
+            lon = 127.073333f;
+        }
+        else if (number == 11)
+        {
+            lat = 37.550996f;
+            lon = 127.075792f;
+        }
+        else if (number == 12)
+        {
+            lat = 37.551479f;
+            lon = 127.075213f;
+        }
+        else if (number == 13)
+        {
+            lat = 37.551845f;
+            lon = 127.074685f;
+        }
+        else if (number == 14)
+        {
+            lat = 37.551631f;
+            lon = 127.074323f;
+        }
+        else if (number == 15)
+        {
+            lat = 37.551821f;
+            lon = 127.074029f;
+        }
+        else if (number == 16)
+        {
+            lat = 37.552255f;
+            lon = 127.074014f;
+        }
+        else if (number == 17)
+        {
+            lat = 37.552442f;
+            lon = 127.073421f;
+        }
+        else if (number == 18)
+        {
+            lat = 37.552545f;
+            lon = 127.074157f;
+        }
+        StartCoroutine(Map());
+    }
+    public void PlusZoom()
+    {
+        zoom++;
+        img = gameObject.GetComponent<RawImage>();
+        StartCoroutine(Map());
+    }
+    public void MinusZoom()
+    {
+        zoom--;
+        img = gameObject.GetComponent<RawImage>();
+        StartCoroutine(Map());
+    }
+    public void SetLat(float dist)
+    {
+        lat += dist;
+        img = gameObject.GetComponent<RawImage>();
+        StartCoroutine(Map());
+    }
+    public void SetLon(float dist)
+    {
+        lon += dist;
+        img = gameObject.GetComponent<RawImage>();
+        StartCoroutine(Map());
+    }
+
+    IEnumerator Map()
+    {
+        url = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon +
+            "&zoom=" + zoom + "&size=" + mapWidth + "x" + mapHeight + "&scale=" + scale
+            + "&maptype=" + mapSelected +
+            "&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyAOl0LVTXiuaSbC_qzmL1rJO5RWbYIrQAo";
+        WWW www = new WWW(url);
+        yield return www;
+        img.texture = www.texture;
+        img.SetNativeSize();
+
+        img.GetComponent<RectTransform>().sizeDelta = new Vector2(1080, 1080);
+
+    }
+    // Use this for initialization
+    void Start()
+    {
+        img = gameObject.GetComponent<RawImage>();
+        StartCoroutine(Map());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
