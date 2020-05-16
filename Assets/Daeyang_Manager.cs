3562f560bd608f166d77c723d961625e47de16f8
bongho cha <ho9354@naver.com>
2020-05-16 16:25:00 +0900

    Add add DaeyangAI Moving
    - Drag and Drop DaeyangAI
    - Link New Contact Page

---
+++ Assets/Daeyang_Manager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Daeyang_Manager : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    RectTransform rectTransform;
+    Vector3 Initial_pos;
+    bool check;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        check = true;
+        Debug.Log("클릭");
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+
+        Initial_pos = rectTransform.anchoredPosition;
+
+        check = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (!check)
+        {
+            rectTransform.anchoredPosition = new Vector2(Mathf.Lerp(rectTransform.anchoredPosition.x, Initial_pos.x, 1), Mathf.Lerp(rectTransform.anchoredPosition.y, Initial_pos.y, 1));
+        }
+        */
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //check = false;
+        rectTransform.anchoredPosition = Initial_pos;
+        Debug.Log("끝");
+    }
+}
